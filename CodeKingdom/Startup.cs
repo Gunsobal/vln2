47f3f1ea6666984cc4df84399acb51c6922d6971
Sæþór Hallgrímsson <saethor@hringdu.is>
2017-05-02 09:37:15 +0000

    Initial commit

---
+++ CodeKingdom/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CodeKingdom.Startup))]
+namespace CodeKingdom
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
