562953316310956a65a636eaea7c871203847276
Gunnar Baldursson <Gunso_Baldurs@hotmail.com>
2017-05-02 16:28:48 +0000

    Added mock data context

---
+++ CodeKingdomTests/MockDataContext.cs
@@ -0,0 +1,45 @@
+﻿using System.Data.Entity;
+using CodeKingdom.Models;
+using CodeKingdom.Models.Entities;
+
+namespace CodeKingdomTests
+{
+    class MockDataContext : IAppDataContext
+    {
+        /// <summary>
+        /// Sets up the fake database.
+        /// </summary>
+        public MockDataContext()
+        {
+            // We're setting our DbSets to be InMemoryDbSets rather than using SQL Server.
+            this.Chats = new InMemoryDbSet<Chat>();
+            this.Collaborators = new InMemoryDbSet<Collaborator>();
+            this.CollaboratorRoles = new InMemoryDbSet<CollaboratorRole>();
+            this.Files = new InMemoryDbSet<File>();
+            this.Folders = new InMemoryDbSet<Folder>();
+            this.Projects = new InMemoryDbSet<Project>();
+            this.UserConfigurations = new InMemoryDbSet<UserConfiguration>();
+        }
+
+        public IDbSet<Chat> Chats { get; set; }
+        public IDbSet<Collaborator> Collaborators { get; set; }
+        public IDbSet<CollaboratorRole> CollaboratorRoles { get; set; }
+        public IDbSet<File> Files { get; set; }
+        public IDbSet<Folder> Folders { get; set; }
+        public IDbSet<Project> Projects { get; set; }
+        public IDbSet<UserConfiguration> UserConfigurations { get; set; }
+
+        public int SaveChanges()
+        {
+            // Pretend that each entity gets a database id when we hit save.
+            int changes = 0;
+
+            return changes;
+        }
+
+        public void Dispose()
+        {
+            // Do nothing!
+        }
+    }
+}
