878fe255cf8208f712e8a1b58eb64ed6274d18b2
Gunnar Baldursson <Gunso_Baldurs@hotmail.com>
2017-05-03 15:33:06 +0000

    Fixed test seed

--- CodeKingdomTests/TestSeed.cs
+++ CodeKingdomTests/TestSeed.cs
@@ -11,41 +11,7 @@ namespace CodeKingdomTests
     {
         static public void All(MockDataContext context)
         {
-            Users(context);
+
         }
-
-        static public void Users(MockDataContext context)
-        {
-            context.Users.Add(new ApplicationUser {
-                Id = "1",
-                UserName = "gunzo@gunzo.is",
-                Email = "gunzo@gunzo.is"
-            });
-
-            context.Users.Add(new ApplicationUser
-            {
-                Id = "2",
-                UserName = "disa@klukka.is",
-                Email = "disa@klukka.is"
-            });
-
-            context.Users.Add(new ApplicationUser
-            {
-                Id = "3",
-                UserName = "saesi@hotmale.is",
-                Email = "saesi@hotmale.is"
-            });
-
-            context.Users.Add(new ApplicationUser
-            {
-                Id = "4",
-                UserName = "stuni@stud.is",
-                Email = "stuni@stud.is"
-            });
-        }
-
-
     }
-  
-  
 }
