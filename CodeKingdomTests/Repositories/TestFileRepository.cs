0335d46f85ebca9074069e12845ee232981e93d1
unnsteinngardars <unnsteinng@gmail.com>
2017-05-04 11:36:28 +0000

    Working on TestSeed Class

---
+++ CodeKingdomTests/Repositories/TestFileRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeKingdom.Repositories;
+
+namespace CodeKingdomTests.Repositories
+{
+    [TestClass]
+    public class TestFileRepository
+    {
+        private FileRepository repo;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            var mockDb = new MockDataContext();
+            TestSeed.Files(mockDb);
+            repo = new FileRepository(mockDb);
+        }
+
+        [TestMethod]
+        public void TestGetById()
+        {
+            // Arrange
+
+            const int id = 1;
+
+            // Act
+
+            var result = repo.GetById(id);
+
+            // Assert
+            Assert.IsNotNull(id);
+        }
+    }
+}
--- CodeKingdomTests/TestSeed.cs
+++ CodeKingdomTests/TestSeed.cs
@@ -14,6 +14,7 @@ namespace CodeKingdomTests
         {
 
         }
+        
         static public void Folders(MockDataContext context)
         {
             /// Root Folders
@@ -22,13 +23,11 @@ namespace CodeKingdomTests
                 ID = 1,
                 Name = "root1"
             });
-
             context.Folders.Add(new Folder
             {
                 ID = 2,
                 Name = "root2"
             });
-
             /// Folders
             context.Folders.Add(new Folder
             {
@@ -36,24 +35,95 @@ namespace CodeKingdomTests
                 Name = "css",
                 FolderID = 1
             });
-
             context.Folders.Add(new Folder
             {
                 ID = 4,
                 Name = "scripts",
                 FolderID = 1
             });
-
             context.Folders.Add(new Folder
             {
                 ID = 5,
-                Name = "jquery",
+                Name = "javascript",
                 FolderID = 4
             });
         }
 
         static public void Files(MockDataContext context)
         {
+            context.Files.Add(new File
+            {
+                ID = 1,
+                Name = "index",
+                FolderID = 1,
+                Type = "html",
+                Content = "stuff",
+                ApplicationUserID = "dummy",
+            });
+            context.Files.Add(new File
+            {
+                ID = 2,
+                Name = "style",
+                FolderID = 3,
+                Type = "css",
+                Content = "css-stuff",
+                ApplicationUserID = "dummy",
+            });
+            context.Files.Add(new File
+            {
+                ID = 3,
+                Name = "myscript",
+                FolderID = 5,
+                Type = "js",
+                Content = "script-stuff",
+                ApplicationUserID = "dummy",
+            });
+            context.Files.Add(new File
+            {
+                ID = 4,
+                Name = "birds",
+                FolderID = 2,
+                Type = "jpg",
+                Content = "pic-of-birds",
+                ApplicationUserID = "dummy",
+            });
+            context.Files.Add(new File
+            {
+                ID = 5,
+                Name = "better-birds",
+                FolderID = 2,
+                Type = "png",
+                Content = "pic-of-better-birds",
+                ApplicationUserID = "dummy",
+            });
+            context.Files.Add(new File
+            {
+                ID = 6,
+                Name = "logo",
+                FolderID = 2,
+                Type = "ai",
+                Content = "illustrator-file",
+                ApplicationUserID = "dummy",
+            });
+        }
+
+        static public void Projects(MockDataContext context)
+        {
+            context.Projects.Add(new Project
+            {
+                ID = 1,
+                Name = "SpaceX",
+                FolderID = 1,
+                Frozen = false
+            });
+
+            context.Projects.Add(new Project
+            {
+                ID = 2,
+                Name = "The new Enron",
+                FolderID = 2,
+                Frozen = false
+            });
 
         }
 
