849a0119c120c689de77762bc532596fdc57e284
disaerna <disa.erna@gmail.com>
2017-05-04 13:20:32 +0000

    EditorViewModel  + Editor + Details View for Project
    EditorViewModel made + Ace editor + view for detail for project
    controller

---
+++ CodeKingdom/Models/ViewModels/EditorViewModel.cs
@@ -0,0 +1,19 @@
+﻿using CodeKingdom.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CodeKingdom.Models.ViewModels
+{
+    public class EditorViewModel
+    {
+        public string Name { get; set; }
+        public int ProjectID { get; set; }
+        public List<Collaborator> Collaborators { get; set; }
+        public List<Folder> Folders { get; set; }
+        public List<File> Files { get; set; }
+    }   
+
+
+}
