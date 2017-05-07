ea6210b8e675b128802f4a3525df60c95881eb69
Sæþór Hallgrímsson <saethor@hringdu.is>
2017-05-07 19:35:31 +0000

    Signal R sends every update to connected clients and saves to the database automatic

---
+++ CodeKingdom/API/EditorHub.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using CodeKingdom.Repositories;
+using CodeKingdom.Models.ViewModels;
+
+namespace CodeKingdom.API
+{
+    public class EditorHub : Hub
+    {
+        private FileRepository fileRepository = new FileRepository();
+
+        public void JoinFile(int fileID)
+        {
+            Groups.Add(Context.ConnectionId, Convert.ToString(fileID));
+        }
+
+        public void OnChange(object data, int fileID)
+        {
+            Clients.Group(Convert.ToString(fileID), Context.ConnectionId).OnChange(data);
+        }
+
+        public void Save(string content, int fileID)
+        {
+            FileViewModel viewModel = new FileViewModel
+            {
+                ID = fileID,
+                Content = content
+            };
+
+            fileRepository.Update(viewModel);
+        }
+
+        public void LeaveFile(int fileID)
+        {
+            Groups.Remove(Context.ConnectionId, Convert.ToString(fileID));
+        }
+    }
+}
