d6613aa8f1d5a39976cb7f6881c402f0a5542c0d
Sæþór Hallgrímsson <saethor@hringdu.is>
2017-05-08 14:06:04 +0000

    new file functionality in root of the project

--- CodeKingdom/Controllers/FileController.cs
+++ CodeKingdom/Controllers/FileController.cs
@@ -16,59 +16,37 @@ namespace CodeKingdom.Controllers
     public class FileController : Controller
     {
         private ApplicationDbContext db = new ApplicationDbContext();
-
         private FileRepository repository = new FileRepository();
+        private ProjectRepository projectRepository = new ProjectRepository();
 
-        // GET: File
-        public ActionResult Index()
+        public ActionResult Create(int? id)
         {
-            var files = db.Files.Include(f => f.Folder).Include(f => f.Owner);
-            return View(files.ToList());
-        }
-
-        // GET: File/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
+            if (!id.HasValue)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            File file = db.Files.Find(id);
-            if (file == null)
+            FileViewModel model = new FileViewModel
             {
-                return HttpNotFound();
-            }
-            return View(file);
+                ProjectID = id.Value
+            };
+            return View(model);
         }
 
-        // GET: File/Create
-        public ActionResult Create()
-        {
-            ViewBag.FolderID = new SelectList(db.Folders, "ID", "Name");
-            ViewBag.ApplicationUserID = new SelectList(db.Users, "Id", "Email");
-            return View();
-        }
-
-        // POST: File/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ID,Name,Content,Type,FolderID,ApplicationUserID")] File file)
+        public ActionResult Create([Bind(Include = "ID,ProjectID,Name,Type")] FileViewModel file)
         {
             if (ModelState.IsValid)
             {
-                db.Files.Add(file);
-                db.SaveChanges();
-                return RedirectToAction("Index");
+                Project project = projectRepository.getById(file.ProjectID);
+                file.FolderID = project.Root.ID;
+                File newfile = repository.Create(file);
+                return RedirectToAction("Details", "Project", new { id = file.ProjectID, fileID = newfile.ID});
             }
 
-            ViewBag.FolderID = new SelectList(db.Folders, "ID", "Name", file.FolderID);
-            ViewBag.ApplicationUserID = new SelectList(db.Users, "Id", "Email", file.ApplicationUserID);
             return View(file);
         }
 
-        // GET: File/Edit/5
         public ActionResult Edit(int? id)
         {
             if (id == null)
@@ -82,8 +60,7 @@ namespace CodeKingdom.Controllers
             {
                 return HttpNotFound();
             }
-            ViewBag.FolderID = new SelectList(db.Folders, "ID", "Name", file.FolderID);
-            ViewBag.ApplicationUserID = new SelectList(db.Users, "Id", "Email", file.ApplicationUserID);
+            
             return View(file);
         }
 
@@ -92,35 +69,42 @@ namespace CodeKingdom.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,Name,Content,Type,FolderID,ApplicationUserID")] File file)
+        public ActionResult Edit([Bind(Include = "ID,Name,Type")] FileViewModel file)
         {
             if (ModelState.IsValid)
             {
-                db.Entry(file).State = EntityState.Modified;
-                db.SaveChanges();
+                repository.Rename(file);
                 return RedirectToAction("Index");
             }
-            ViewBag.FolderID = new SelectList(db.Folders, "ID", "Name", file.FolderID);
-            ViewBag.ApplicationUserID = new SelectList(db.Users, "Id", "Email", file.ApplicationUserID);
+            
             return View(file);
         }
 
-        // GET: File/Delete/5
         public ActionResult Delete(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            File file = db.Files.Find(id);
+
+            File file = repository.GetById(id.Value);
+
             if (file == null)
             {
                 return HttpNotFound();
             }
-            return View(file);
+
+            FileViewModel model = new FileViewModel
+            {
+                ID = file.ID,
+                Name = file.Name,
+                Type = file.Type,
+                Content = file.Content,
+            };
+
+            return View(model);
         }
 
-        // POST: File/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
